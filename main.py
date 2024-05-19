77f8dd1b04beec7c4e158ee8d2258230a684a4e0
Sylwester Sojka <sylwestersojka@Sylwesters-MacBook-Air.local>
2024-05-19 12:33:28 +0200

    Working column list functionality with removed white spaces -> next step is to iterate over each column for 1 row

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
 import pandas as pd
 import re
+import logging
 
-df = pd.read_excel('truckroll.xlsx')
-# print(df.columns)
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='\n%(levelname)s: %(message)s\n',
+    datefmt='%Y-%m-%d %H:%M:%S',
+    handlers=[
+        logging.FileHandler('app.log'),
+        logging.StreamHandler()
+    ]
+)
 
-x = df.loc[0]
+def get_column_names_to_list(df) -> list:
+    logging.info('get_column_names_to_list function started')
+    output_list = []
+
+    column_names = list(df.columns)
+    for element in column_names:
+        no_new_line_element = element.replace('\n', ' ')
+        output_list.append(no_new_line_element)
+
+    return output_list
 
 
 def get_number_of_rows(df):
@@ -12,25 +29,25 @@ def get_number_of_rows(df):
     print(f'Number of rows is: {number_of_rows}')
     return number_of_rows
     
-
-def get_ticket_id(df_row):
+#TODO: column needs to be specified by user
+def get_column_content(df_row):
+    logging.info('get_column_content function started')
     for column, value in df_row.items():
-        if column == 'Details':
+        if column == 'Nokia Ticket':
             # print(f'Column: {column} value: {value}')
             entry = value
             return entry
 
 
-value = get_ticket_id(x)
-
 def remove_non_alpha(user_string):
-
+    # logging.info('remove_non_alpha function started')
     clean_string = re.sub('[^0-9a-zA-Z]+', '', user_string)
     return clean_string
 
 
 
 def dict_creation_from_value(value_from_column):
+    logging.info('dict_creation_from_value function started')
     split_values_to_dict = {}
     lines = value_from_column.split('\n')
 
@@ -57,10 +74,18 @@ def dict_creation_from_value(value_from_column):
     return split_values_to_dict
         
 
+df = pd.read_excel('truckroll.xlsx')
+first_row_df = df.loc[0]
+value = get_column_content(first_row_df)
 
 
-x = dict_creation_from_value(value)
-for key, value in x.items():
+first_row_df_dict = dict_creation_from_value(value)
+for key, value in first_row_df_dict.items():
     print(f"Key is: {key} with value: {value}")
 
 
+
+list_col_names = get_column_names_to_list(df)
+
+# for element in list_col_names:
+#     print(f' Element: {element}')
